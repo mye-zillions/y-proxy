5c0d7f0c8d3b3c44fafdf468e6adb4d80545b3ca
Leighton Chen <leighton@asia.com>
2019-02-19 16:06:08 -0800

    Render my own service, along with placeholder component images.

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const port = 8888;
+
+app.use(morgan('dev'));
+app.use(express.static('client'));
+
+app.listen(port);
