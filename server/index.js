28b50f266a0778c94b4c0e11883d7ce66e72981a
Yvonne <>
2019-03-08 19:25:10 -0800

    Add proxy middleware

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+var proxy = require('http-proxy-middleware');
 
 const app = express();
 const port = 8888;
 
 app.use(morgan('dev'));
+app.use('/api/properties/', proxy({ target: 'http://localhost:8081', changeOrigin: true}))
 app.use('/:id', express.static('client'));
 
 app.listen(port);
+
+//forward any requests from proxy to service server 
+
